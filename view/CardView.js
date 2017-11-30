8df2a45d59a9cf3db279dafabe969e257c25255b
Ivan Domaranskiy <h2870132@gmail.com>
2017-11-30 11:47:54 +0200

    prepared to draw cards

--- view/CardView.js
+++ view/CardView.js
@@ -21,6 +21,8 @@ CardView.prototype = {
     getControl: function () {
         this.startBtn = document.getElementById('startBtn');
         this.pauseBtn = document.getElementById('pauseBtn');
+        this.puzzleBody = document.getElementById('puzzleBody');
+        this.timer = document.getElementById('timer');
         this.sizeBtns = document.querySelectorAll('.selectSize input[name=size]');
         this.cards = document.getElementsByClassName('card');
 
@@ -28,11 +30,35 @@ CardView.prototype = {
     },
 
     setupHandlers: function () {
+        this.startBtnHandler = this.startBtnMeth.bind(this);
+        this.sizeBtnsHandler = this.sizeBtnsMeth.bind(this);
+
+        /* Event Dispatcher */
+
         return this;
     },
 
     linkEvents: function () {
+        this.startBtn.addEventListener('click', this.startBtnHandler);
+        for (var sizeBtn of this.sizeBtns) {
+            sizeBtn.addEventListener('click', this.sizeBtnsHandler);
+        }
+
+        /* Event Dispatcher */
+
         return this;
+    },
+
+    startBtnMeth: function () {
+        console.log('you pressed startBtn'); // delete later
+        this.startBtnEvent.notify({});
+    },
+
+    sizeBtnsMeth: function () {
+        console.log('the size you have selected is ' + event.target.getAttribute('size')); // delete later
+        this.selectSizeEvent.notify({
+            size: event.target.getAttribute('size')
+        });
     }
 
 };
